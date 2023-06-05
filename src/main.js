2e9f0dfe50de4210094abd1c6742c68efcfefb9c
whiteHawk <freeidlll6@gmail,com>
2023-06-05 21:04:20 +0530

    fixed normal bugs

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/reset.css";
+import "../styles/components/hero.css";
+import "../styles/components/about.css";
+import "../styles/utils.css";
